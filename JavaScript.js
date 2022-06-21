014addadff81e041b8c130d4458880f7350250b8
Ramesh8277 <ramesh.xworkz@gmail.com>
2022-06-21 17:38:27 +0530

    HTML

---
+++ JavaScript.js
@@ -0,0 +1,36 @@
+console.log("Java Script")
+            var a=10;
+            console.log(a)
+            console.log(typeof(a))
+            a="java Script"
+            console.log(a)
+            console.log(typeof(a))
+            var b;
+            console.log(b)
+            console.log(b)
+            console.log(typeof(b))
+            var a;
+            console.log(a)
+
+            a=true;
+            console.log(a)
+            console.log(typeof(a))
+        
+            function disp(){
+                console.log("calling display function")
+                var displayMessage = document.getElementById("Display")
+                console.log(displayMessage)
+                displayMessage.innerHTML ="calling display function"
+            }
+        function sum(){
+            console.log("calling sum")
+            var num1=document.getElementById("num1")
+            console.log(num1)
+            var num2=document.getElementById("num2")
+            console.log(num2)
+            console.log(num1.value)
+            console.log(num2.value)
+            var c=parseInt(num1.value)+parseInt(num2.value)
+            var result=document.getElementById("result")
+            result.innerHTML=c
+        }
---
+++ Taskjs2.js
@@ -0,0 +1,32 @@
+console.log("Java Script Task1")
+            var a=100;
+            console.log(a)
+            console.log(typeof(a))
+            
+            a="xworkz btm "
+            console.log(a)
+            console.log(typeof(a))
+           
+            var b;
+            console.log(b)
+            console.log(typeof(b))
+           
+            R=true;
+            console.log(R)
+            console.log(typeof(R))
+              
+            
+            function print(){
+                console.log("calling print function form java script")
+            }
+
+            function disp(){
+                console.log("calling display function form java script")
+            }
+
+            function ram(){
+                console.log("calling ram function form java script")
+            }
+            function ramesh(){
+                console.log("calling ramesh function form java script")
+            }
